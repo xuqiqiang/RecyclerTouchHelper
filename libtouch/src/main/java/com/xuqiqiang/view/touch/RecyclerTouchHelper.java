8a6e98af3ddcded6c6aa8670440f6010ea9697e6
xuqiqiang <1239749058@qq.com>
2020-09-30 13:02:15 +0800

    Fix: error

--- libtouch/src/main/java/com/xuqiqiang/view/touch/RecyclerTouchHelper.java
+++ libtouch/src/main/java/com/xuqiqiang/view/touch/RecyclerTouchHelper.java
@@ -30,6 +30,8 @@ public class RecyclerTouchHelper {
     private RecyclerView.ViewHolder mSelectViewHolder;
     private Adapter mAdapter;
     private boolean mResort;
+    private int mFromPosition;
+    private int mCurPosition;
 
     public RecyclerTouchHelper(RecyclerView recyclerView) {
         this.mRecyclerView = recyclerView;
@@ -43,6 +45,8 @@ public class RecyclerTouchHelper {
             @Override
             public void onItemLongClick(int position, RecyclerView.ViewHolder vh) {
                 if (mAdapter != null && mAdapter.isEnabled(vh)) {
+                    mFromPosition = vh.getAdapterPosition();
+                    mCurPosition = -1;
                     mItemTouchHelper.startDrag(vh);
                     Vibrator vib = (Vibrator) mContext.getSystemService(Service.VIBRATOR_SERVICE);
                     vib.vibrate(50);
@@ -77,8 +81,11 @@ public class RecyclerTouchHelper {
                 if (!mResort) return true;
                 int fromPosition = viewHolder.getAdapterPosition();
                 int toPosition = target.getAdapterPosition();
-                if (mAdapter != null)
-                    mAdapter.onItemMove(fromPosition, toPosition);
+                if (mAdapter != null) {
+                    toPosition = mAdapter.onItemMove(fromPosition, toPosition);
+                    if (toPosition >= 0)
+                        mCurPosition = toPosition;
+                }
                 return true;
             }
 
@@ -172,14 +179,29 @@ public class RecyclerTouchHelper {
                 public void onDelete(boolean isDelete) {
                     if (isDelete && mAdapter.onDelete(mSelectViewHolder))
                         return;
-                    e.setLocation(e.getX(), 0);
-                    e.setAction(MotionEvent.ACTION_DOWN);
+//                    e.setLocation(e.getX(), 0);
+//                    e.setAction(MotionEvent.ACTION_DOWN);
+
+                    e.setAction(MotionEvent.ACTION_CANCEL);
                     listener.dispatchTouchEvent(e);
+                    restoreSequence();
                 }
             });
             return true;
         }
         return false;
+    }
+
+    private void restoreSequence() {
+        if (mCurPosition >= 0 && mFromPosition >= 0 && mCurPosition != mFromPosition) {
+            mAdapter.onItemMove(mCurPosition, mFromPosition);
+//            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    mAdapter.onItemMove(mCurPosition, mFromPosition);
+//                }
+//            }, 200);
+        }
     }
 
     public interface OnDeleteCallback {
@@ -212,6 +234,7 @@ public class RecyclerTouchHelper {
                 mDataList.remove(mDataList.size() - 1);
                 RecyclerView.Adapter adapter = recyclerView.getAdapter();
                 if (adapter != null) adapter.notifyDataSetChanged();
+//                    adapter.notifyItemRemoved(mDataList.size() - 1);
             } else {
                 onItemMove(viewHolder.getAdapterPosition(), mDataList.size() - 1);
                 viewHolder.itemView.setVisibility(View.INVISIBLE);
@@ -221,15 +244,16 @@ public class RecyclerTouchHelper {
                         mDataList.remove(mDataList.size() - 1);
                         RecyclerView.Adapter adapter = recyclerView.getAdapter();
                         if (adapter != null) adapter.notifyDataSetChanged();
+//                            adapter.notifyItemRemoved(mDataList.size() - 1);
                     }
                 }, 300);
             }
             return true;
         }
 
-        public void onItemMove(int fromPosition, int toPosition) {
+        public int onItemMove(int fromPosition, int toPosition) {
             List mDataList = getDataList();
-            if (Utils.isEmpty(mDataList)) return;
+            if (Utils.isEmpty(mDataList)) return -1;
 
             if (fromPosition < 0) fromPosition = 0;
             else if (fromPosition >= mDataList.size())
@@ -250,6 +274,26 @@ public class RecyclerTouchHelper {
             }
             RecyclerView.Adapter adapter = recyclerView.getAdapter();
             if (adapter != null) adapter.notifyItemMoved(fromPosition, toPosition);
+            return toPosition;
         }
+
+//        private void restoreSequence(int fromPosition, int toPosition) {
+////        int fromPosition = mFromPosition;
+////        int toPosition = mCurPosition;
+//            List mDataList = getDataList();
+//            if (Utils.isEmpty(mDataList)) return;
+//
+//            if (fromPosition < toPosition) {
+//                for (int i = toPosition; i > fromPosition; i--) {
+//                    Collections.swap(mDataList, i, i - 1);
+//                }
+//            } else {
+//                for (int i = fromPosition; i > toPosition; i--) {
+//                    Collections.swap(mDataList, i, i - 1);
+//                }
+//            }
+//            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+//            if (adapter != null) adapter.notifyItemMoved(fromPosition, toPosition);
+//        }
     }
 }
